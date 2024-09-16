f5a4c34c0cd57fb4fa1da4501448c008c02678df
guboc11 <guboc11@gmail.com>
2024-09-16 19:22:51 +0900

    Add WalletProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { WalletProvider, SuiDevnetChain } from "@suiet/wallet-kit";
 
 type PageComponentProps = {
   title: string;
@@ -11,18 +12,40 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const PageComponent = Component as React.ComponentType<PageComponentProps>;
 
+  const suiTestnetChain = {
+		id: "sui:testnet",
+		name: "Sui Testnet",
+		rpcUrl: "https://sui-testnet.nodeinfra.com",
+	};
+
   // Check if the current route is the landing page
   const isLandingPage = router.pathname === "/landing";
   const isSetProfilePage = router.pathname === "/setprofile";
 
   if (isLandingPage || isSetProfilePage) {
-    return <PageComponent {...pageProps} />;
+    return (
+      <WalletProvider
+        chains={[
+          SuiDevnetChain,
+          suiTestnetChain,
+        ]}
+      >
+        <PageComponent {...pageProps} />
+      </WalletProvider>
+    );
   }
 
   return (
-    <Layout title={pageProps.title || "Near and Dear"}>
-      <PageComponent {...pageProps} />
-    </Layout>
+    <WalletProvider
+      chains={[
+        SuiDevnetChain,
+        suiTestnetChain,
+      ]}
+    >
+      <Layout title={pageProps.title || "Near and Dear"}>
+          <PageComponent {...pageProps} />
+      </Layout>
+    </WalletProvider>
   );
 }
 
